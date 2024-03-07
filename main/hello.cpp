e4cec171815e21136d34366647c42ad8fc0aba22
Kaashika <agr.kaashika@gmail.com>
2024-03-07 20:50:11 +0530

    commit 2

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World updated\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
